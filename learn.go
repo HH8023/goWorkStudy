075bedb1661ce4721a30828b642aa11648422d97
HH88023 <wp_0357@163.com>
2019-06-27 23:22:47 +0800

    const(常量和枚举的定义)

---
+++ learn.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+const NAME  = "book"
+
+var (
+    v1 int
+    v2 string
+)
+var mainName  = "main name"
+func main() {
+    fmt.Print(v1,"111\n")
+    fmt.Print(v2,"222")
+
+   // fmt.Println("learn1")
+   //fmt.Print(mainName)
+   //fmt.Print(NAME)
+}
